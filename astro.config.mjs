0f7f61f6f9a1247f1a7a3031bb8fb9cff2d8f6e6
tibitTW <chunhua.2147483647@gmail.com>
2024-11-30 14:42:14 +0800

    style: change format of some files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'IKTXY docs',
-			social: {
-				github: 'https://github.com/iktxy',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-    ],
     base: "/iktxy-docs/",
+    integrations: [
+        starlight({
+            title: "IKTXY docs",
+            social: {
+                github: "https://github.com/iktxy",
+            },
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: "Example Guide", slug: "guides/example" },
+                    ],
+                },
+                {
+                    label: "Reference",
+                    autogenerate: { directory: "reference" },
+                },
+                {
+                    label: "Specs",
+                    autogenerate: { directory: "specs" },
+                },
+                {
+                    label: "Meeting Notes",
+                    autogenerate: { directory: "meeting-notes" },
+                },
+            ],
+        }),
+    ],
 });
